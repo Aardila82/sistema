94015315fd9d2d4e028cbff2717cef7564bfc90a
Alexander <desarrollador1@arsuarez.com>
2025-05-23 16:43:56 -0500

    feat: HU 80033244 - Creacion de controlador y vista estado con tabla

--- sistema/app/Http/Controllers/EntradaController.php
+++ sistema/app/Http/Controllers/EntradaController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Entrada;
 
 class EntradaController extends Controller
 {
     public function index(){
-        return view('entrada.index');
+        $entradas = Entrada::all();
+        return view('entrada.index', compact('entradas'));
     }
 }
--- sistema/resources/views/entrada/index.blade.php
+++ sistema/resources/views/entrada/index.blade.php
@@ -7,5 +7,23 @@
 </head>
 <body>
     <h2>Entradas</h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Tag</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($entradas as $entrada)
+                <tr>
+                    <td>{{ $entrada->id }}</td>
+                    <td>{{ $entrada->titulo }}</td>
+                    <td>{{ $entrada->tag }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
